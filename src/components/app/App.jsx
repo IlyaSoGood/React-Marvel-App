22d5044d8284ae1589216db08f2e6cdf845a97c8
Ilya Ermolin <tomixlove@gmail.com>
2023-05-29 18:31:59 +0300

    react-router v6

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,45 +1,31 @@
-import { useState } from 'react';
-
 import AppHeader from '../appHeader/AppHeader';
-import RandomChar from '../randomChar/RandomChar';
-import CharList from '../charList/CharList';
-import CharInfo from '../charInfo/CharInfo';
-import AppBanner from '../appBanner/AppBanner';
-import ComicsList from '../comicsList/ComicsList';
+
 import SingleComic from '../singleComic/SingleComic';
-import ErrorBoundary from '../errorBoundary/ErrorBoundary';
+
+import { MainPage, ComicsPage } from '../pages';
+import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
 
 import './App.scss';
-import vision from '../../resources/img/vision.png';
+
 
 const App = () => {
-    const [selectedChar, setChar] = useState(null);
-
-    const onCharSelected = (id) => {
-        setChar(id);
-    }
 
     return (
-        <div className="app">
-            <AppHeader/>
-            {/* <main>
-                <ErrorBoundary>
-                    <RandomChar/>
-                </ErrorBoundary>
-                <div className='char__content'>
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected}/>
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={vision} alt="vision"></img>
-            </main> */}
-            <ErrorBoundary>
-                <ComicsList/>
-            </ErrorBoundary>
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Routes>
+                        <Route path='/' element={<MainPage/>} />
+        
+                        <Route path='/comics' element={<ComicsPage/>} />
+                    </Routes>
+
+
+                </main>
+            </div>
+        </Router>
+
     );
 
 
---
+++ src/components/pages/MainPage.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import RandomChar from '../randomChar/RandomChar';
+import CharList from '../charList/CharList';
+import CharInfo from '../charInfo/CharInfo';
+import ErrorBoundary from '../errorBoundary/ErrorBoundary';
+
+import vision from '../../resources/img/vision.png';
+
+const MainPage = () => {
+    const [selectedChar, setChar] = useState(null);
+
+    const onCharSelected = (id) => {
+        setChar(id);
+    }
+
+    return (
+        <div>
+            <ErrorBoundary>
+                <RandomChar/>
+            </ErrorBoundary>
+            <div className='char__content'>
+                <ErrorBoundary>
+                    <CharList onCharSelected={onCharSelected}/>
+                </ErrorBoundary>
+                <ErrorBoundary>
+                    <CharInfo charId={selectedChar}/>
+                </ErrorBoundary>
+            </div>
+            <img className="bg-decoration" src={vision} alt="vision"></img>
+        </div>
+    );
+};
+
+export default MainPage;
