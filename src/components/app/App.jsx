2829a2f0e2b663a6bc57f68ca4b24426af474541
Ilya Ermolin <tomixlove@gmail.com>
2023-06-14 12:17:19 +0300

    Шаблон страницы с comic вынесен как общий, другие страницы через render props

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -10,8 +10,10 @@ import './App.scss';
 const Page404 = lazy(() => import('../pages/404'));
 const MainPage = lazy(() => import('../pages/MainPage'));
 const ComicsPage = lazy(() => import('../pages/ComicsPage'));
-const SingleComicpage = lazy(() => import('../pages/SingleComicPage'));
-const SingleCharacterPage = lazy(() => import('../pages/SingleCharacterPage'));
+
+const SingleComicLayout = lazy(() => import('../pages/singleComicLayout/SingleComicLayout'));
+const SingleCharacterLayout = lazy(() => import('../pages/singleCharacterLayout/SingleCharacterLayout'));
+const SinglePage = lazy(() => import('../pages/SinglePage'));
 
 const App = () => {
 
@@ -26,9 +28,9 @@ const App = () => {
             
                             <Route path='/comics' element={<ComicsPage/>} />
 
-                            <Route path='/comics/:comicId' element={<SingleComicpage/>} />
+                            <Route path='/comics/:id' element={<SinglePage Component={SingleComicLayout} dataType='comic'/>} />
 
-                            <Route path='/character/:characterId' element={<SingleCharacterPage/>} />
+                            <Route path='/characters/:id' element={<SinglePage Component={SingleCharacterLayout} dataType='character'/>} />
 
                             <Route path="*" element={<Page404/>} />
 
